00ecbb3317d6edbd97d0a2dabc405ba314a6ef7e
Ashire160 <ashire1600@gmail.com>
2024-01-19 13:07:21 -0500

    Initial Commit

---
+++ src/gitdemo1/GitDemo1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitdemo1;
+
+/**
+ *
+ * @author a00
+ */
+public class GitDemo1 {
+
+    /**
+     * @modified
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World1 + changes");
+    }
+    
+}
